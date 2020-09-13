fb580daab9dbf8086e8cae71b6d1baaf980e4bb7
Jason D'Oyley <jdfd.dev@icloud.com>
2020-09-13 16:20:01 -0400

    Change direction if indent is 0 or 20

--- zig_zag.py
+++ zig_zag.py
@@ -6,16 +6,27 @@ isincrease = True  # check if to increase the indentation or not
 
 try:
     while True:
-        # TODO print indentation and then eight asterisks (*) to the screen
+        # Print indentation and then eight asterisks (*) to the screen
         print(' ' * indent_by, end='')
         print('********')
 
-        # TODO: Pause program for 0.1s
+        # Pause program for 0.1s
         time.sleep(0.1)
 
-        # TODO: Increase the number of indents
-        indent_by += 1
+        if isincrease:
+            # Increase the number of indents
+            indent_by += 1
+
+            if indent_by == 20:
+                isincrease = False
+        else:
+            # Decrease if isincrease is false
+            indent_by -= 1
+
+            if indent_by == 0:
+                isincrease = True
 except KeyboardInterrupt:
-    # Code to execute in the event of an exception
     # Exit program when the stop button or CTRL-C is pressed
     sys.exit()
+
+print('CTRL-C Pressed!')
