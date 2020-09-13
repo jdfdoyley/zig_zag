e64790cc68bbc8642e397afa4577c2c9ad4e9545
Jason D'Oyley <jdfd.dev@icloud.com>
2020-09-13 16:20:01 -0400

    Change direction if indent is 0 or 20

--- zig_zag.py
+++ zig_zag.py
@@ -1,21 +1,36 @@
+"""
+Author: Jason D'Oyley
+Date: Sept. 13, 2020
+Source: Automate the Boring Stuff with Python by Al Sweigart
+"""
+
 import time
 import sys
 
-indent_by = 0  # how many spaces to indent
-isincrease = True  # check if to increase the indentation or not
+indent_by = 0  # How many spaces to indent
+isincrease = True  # Check if to increase the indentation or not
 
 try:
     while True:
-        # TODO print indentation and then eight asterisks (*) to the screen
+        # Print indentation and then eight asterisks (*) to the screen
         print(' ' * indent_by, end='')
         print('********')
 
-        # TODO: Pause program for 0.1s
+        # Pause program for 0.1s
         time.sleep(0.1)
 
-        # TODO: Increase the number of indents
-        indent_by += 1
+        if isincrease:
+            # Increase the number of indents
+            indent_by += 1
+
+            if indent_by == 20:
+                isincrease = False
+        else:
+            # Decrease if isincrease is false
+            indent_by -= 1
+
+            if indent_by == 0:
+                isincrease = True
 except KeyboardInterrupt:
-    # Code to execute in the event of an exception
     # Exit program when the stop button or CTRL-C is pressed
     sys.exit()
