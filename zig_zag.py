302f473d00b028b5389c0fb78e27e992bc53558e
Jason D'Oyley <jdfd.dev@icloud.com>
2020-09-13 16:11:26 -0400

    Refactor to add indentation [zig_zag]

--- zig_zag.py
+++ zig_zag.py
@@ -5,7 +5,17 @@ indent_by = 0  # how many spaces to indent
 isincrease = True  # check if to increase the indentation or not
 
 try:
-    # Place code to try here...
+    while True:
+        # TODO print indentation and then eight asterisks (*) to the screen
+        print(' ' * indent_by, end='')
+        print('********')
+
+        # TODO: Pause program for 0.1s
+        time.sleep(0.1)
+
+        # TODO: Increase the number of indents
+        indent_by += 1
 except KeyboardInterrupt:
     # Code to execute in the event of an exception
     # Exit program when the stop button or CTRL-C is pressed
+    sys.exit()
