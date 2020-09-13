71aabe5c253e2d7bd42532644f5aa4734b812632
Jason D'Oyley <jdfd.dev@icloud.com>
2020-09-13 16:04:41 -0400

    Create program outline [zig_zag]

---
+++ zig_zag.py
@@ -0,0 +1,11 @@
+import time
+import sys
+
+indent_by = 0  # how many spaces to indent
+isincrease = True  # check if to increase the indentation or not
+
+try:
+    # Place code to try here...
+except KeyboardInterrupt:
+    # Code to execute in the event of an exception
+    # Exit program when the stop button or CTRL-C is pressed
